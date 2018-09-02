7984ae3ac1362a092632d9fe2c14753cfa93bd63
khuxkm <khuxkm@tilde.team>
2018-09-02 15:42:50 -0400

    Generate index.html

--- compile.py
+++ compile.py
@@ -8,9 +8,14 @@ env = Environment(loader=FileSystemLoader("./templates/"))
 env.filters["markdown"]=markdownfilter
 temp = env.get_template("article.html")
 
+if not os.path.exists("build"):
+	os.system("mkdir build; cp style.css build")
+
 articles = os.listdir("articles")
 articles = [[os.path.splitext(x)[0],frontmatter.load("articles/"+x)] for x in articles]
 for article in articles:
 	if article[1]["published"]:
-		with open(article[0]+".html","w") as f:
+		with open("build/"+article[0]+".html","w") as f:
 			f.write(temp.render(article=article[1]))
+with open("build/index.html","w") as f:
+	f.write(env.get_template("mainpage.html").render(articles=articles))
