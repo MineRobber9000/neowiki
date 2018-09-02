45cf0097bd825f34291ed507fbdbb47d22cbab1c
khuxkm <khuxkm@tilde.team>
2018-09-02 15:30:17 -0400

    Make compiler compile pages

--- compile.py
+++ compile.py
@@ -1,8 +1,16 @@
 import os, frontmatter, markdown
+from jinja2 import Environment, FileSystemLoader, contextfilter, Markup
+
+def markdownfilter(val):
+	return Markup(markdown.markdown(val))
+
+env = Environment(loader=FileSystemLoader("./templates/"))
+env.filters["markdown"]=markdownfilter
+temp = env.get_template("article.html")
 
 articles = os.listdir("articles")
 articles = [[os.path.splitext(x)[0],frontmatter.load("articles/"+x)] for x in articles]
 for article in articles:
 	if article[1]["published"]:
 		with open(article[0]+".html","w") as f:
-			f.write(markdown.markdown(article[1].content))
+			f.write(temp.render(article=article[1]))
