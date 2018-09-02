bd7dfe97a51024ef7a405279c2e2e42dd8f8c604
khuxkm <khuxkm@tilde.team>
2018-09-02 14:47:41 -0400

    Add stylesheet

--- header.php
+++ header.php
@@ -3,6 +3,7 @@
 <head>
 	<title>neowiki | <?=($title ?? 'better than Penny\'s Pages!')?></title>
 	<link href="https://tilde.team/css/fork-awesome.css" rel="stylesheet">
+	<link href="style.css" rel="stylesheet">
 
 	<?=$additional_head?>
 </head>
