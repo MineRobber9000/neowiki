0b35804cc17538a422d5941e8cdbe3cba16707d0
khuxkm <khuxkm@tilde.team>
2018-09-05 16:25:32 -0400

    Add logo to assets

--- compile.py
+++ compile.py
@@ -16,7 +16,7 @@ pubdir = config["neowiki"]["published_path"]
 temp = env.get_template("article.html")
 
 if not os.path.exists("build"):
-	os.system("mkdir build; cp style.css build")
+	os.system("mkdir build; cp assets/* build")
 
 articles = os.listdir("articles")
 articles = [[os.path.splitext(x)[0],frontmatter.load("articles/"+x)] for x in articles]
