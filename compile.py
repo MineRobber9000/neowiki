b8a37e9ce8f62ee312d88a78cc095f4e1de87c22
khuxkm fbexl <khuxkm@ttm.sh>
2018-09-15 00:56:22 -0400

    Add contributors page
    Now to remove authorship from the other pages.

--- compile.py
+++ compile.py
@@ -10,8 +10,11 @@ env.filters["markdown"]=markdownfilter
 config = configparser.ConfigParser()
 config.read("config.ini")
 assert "neowiki" in config.sections(),"Invalid config file: must have neowiki section"
-assert "published_path" in config["neowiki"],"Invalid config file: must have published_path directive under neowiki section"
-pubdir = config["neowiki"]["published_path"]
+config = config["neowiki"]
+assert "published_path" in config,"Invalid config file: must have published_path directive under neowiki section"
+pubdir = config["published_path"]
+assert "contributors" in config,"Invalid config file: must have contributors list under neowiki section"
+contributors = config["contributors"].split(",")
 
 temp = env.get_template("article.html")
 
@@ -26,3 +29,5 @@ for article in articles:
 			f.write(temp.render(article=article[1],filename=article[0],pubdir=pubdir))
 with open("build/index.html","w") as f:
 	f.write(env.get_template("mainpage.html").render(articles=articles,pubdir=pubdir))
+with open("build/contributors.html","w") as f:
+	f.write(env.get_template("contributors.html").render(contributors=contributors))
